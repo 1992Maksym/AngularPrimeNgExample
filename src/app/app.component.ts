cd3cdcb02ca8c0bf26d3d4313e56f36e56bf4609
Maksym Salov <maksimsalov8@gmail.com>
2022-07-04 14:55:19 +0300

    start with PrimeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { map, tap, } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularPrimeNGExample';
+export class AppComponent implements OnInit{
+
+  basicData: any;
+  heroesName:any = [];
+  heroesHeight:any = [];
+
+  constructor(private http: HttpClient) {}
+  getData(){
+    return this.http.get('https://swapi.dev/api/people').pipe(
+      map((value: any) => {
+        return value['results'].map((el: any) => {
+          this.heroesName.push(el.name);
+          this.heroesHeight.push(el.height);
+        });
+      }),
+      tap(el => {
+        this.basicData = {
+          labels: this.heroesName,
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: '#42A5F5',
+              data: this.heroesHeight,
+            }
+          ]
+        };
+      })
+    ).subscribe()
+  }
+  ngOnInit() {
+    this.getData()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartModule } from 'primeng/chart';
+import { HttpClientModule } from "@angular/common/http";
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ChartModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
